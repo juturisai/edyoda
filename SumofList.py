009f4cda4d760bd4a1dd3daa78584bb0a0c94059
juturisai <95975191+juturisai@users.noreply.github.com>
2021-12-29 20:40:25 +0530

    Create SumofList.py

---
+++ SumofList.py
@@ -0,0 +1,9 @@
+def sumlistvalues(l):
+    sum = 0
+    for i in l:
+        sum+= i
+    return sum
+        
+inputstring = input("enter integers : ").split(',') 
+lists = list(map(int, inputstring))
+print(sumvalues(lists))
